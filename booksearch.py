2fe9b123170f34acf85cf7f2c86740a83ade7b1f
片田 猛士 <katada-t@hi-ad.jp>
2019-03-15 17:57:12 +0900

    本検索のようなもの

--- booksearch.py
+++ booksearch.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 
 myapp = Flask(__name__)
 
-@myapp.route('/booksearch', methods=['GET'])
-def booksearch():
-    title = request.args.get('title')
-    author = request.args.get('author')
-    return '{1}さんの本「{0}」ですね。お探しします'.format(title,author)
+@myapp.route('/searchform', methods=['GET'])
+def searchform():
+    renderpage = render_template("searchform.html")
+    return renderpage
+
+@myapp.route('/result', methods=['POST'])
+def show_result():
+    title = request.form['title']
+    author = request.form['author']
+    the_result = '<p>{1}さんの本[{0}]ですね。お探しします</p>'.format(title,author)
+    link_back = '<p><a href="/searchform">検索フォームへ</a>'
+    return the_result + link_back
